07cd2a74e8fa633c850eaa53124f67f99ac1c93f
Jorge De Gante Tellez <jdegante@plct.com.mx>
2011-06-14 10:44:57 -0500

    commit inicial

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# Ruby on Rails Tutorial: Sample App
+
+Aplicación de ejemplo [*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.org/) por [*Jorge De Gante Tellez*] <jdegante@plct.com.mx>
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '7125c02f1249b5dd2fd51eb5dbfd2647b19409d05aa30531c1d84ae2210babad591e98fe08d14d7d916fcf3fa2669f620376464c16fdc7af922b9c07bc331cf2'
